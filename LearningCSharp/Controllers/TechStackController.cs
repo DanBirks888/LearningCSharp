b1e8f7be08b495bbb9d57ae006ab001477160a49
Dan Birks <danbirks888@gmail.com>
2021-02-21 11:56:47 +0000

    Creating Poco's & Controller + Git Push (Mac)

---
+++ LearningCSharp/Controllers/TechStackController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LearningCSharp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LearningCSharp.Controllers
+{
+    public class TechStackController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var ts = new TechStack() {
+                name = "Java",
+                UsedBefore = true,
+                Proficiency = 7,
+                FavAtrributes = { "Intellij", "Themes", "Spring Boot", "Syntax Layout" } };
+            return View(ts);
+        }
+    }
+}
---
+++ LearningCSharp/Models/TechStack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningCSharp.Models
+{
+    public class TechStack
+    {
+        public string name { get; set; }
+        public bool UsedBefore { get; set; }
+        public int Proficiency { get; set; }
+        public List<string> FavAtrributes { get; set; }
+    }
+}
