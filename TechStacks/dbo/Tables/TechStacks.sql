0c662d8532582567f1550f6689173c8f33c1dbae
Dan <danbirks888@gmail.com>
2021-02-22 16:44:44 +0000

    SQL DB creation. Cannot resolve location or creation of Web.config to create connection string

---
+++ TechStacks/dbo/Tables/TechStacks.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TechStacks]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Name] NCHAR(10) NOT NULL, 
+    [UsedBefore] BIT NOT NULL, 
+    [Proficiency] INT NOT NULL, 
+    [FavAttribute] NCHAR(10) NOT NULL
+)
